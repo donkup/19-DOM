66e9fc000370f3e3b16a6f35be2feedf76cd165d
Donatas <donkuprys@gmail.com>
2021-06-07 20:59:05 +0300

    10 11 12 uzdavinys

--- js/main.js
+++ js/main.js
@@ -1,20 +1,31 @@
-const inputDOM = document.querySelector('input');
-const buttonDOM = document.querySelector('button');
-const spanDOM = document.querySelector('span');
+const submitDOM = document.querySelector('button');
+const orderDOM = document.querySelector('.order');
+const nameDOM = document.querySelector('#customer');
+const sriubaDOM = document.querySelector('#sriuba');
+const pagrindinisPatiekalasDOM = document.querySelector('#patiekalas');
+const desertasDOM = document.querySelector('#desertas');
+const allGerimaiDOM = document.querySelectorAll('input[name="gerymas"]');
+
+function arNoriu(DOM) {
+    return DOM.checked ? 'nori' : 'nenori';
+}
+
+function kurisPazymetas(DOMlist) {
+    for (const itemDOM of DOMlist) {
+        if (itemDOM.checked) {
+            return itemDOM.value;
+        }
+    }
+}
+
+submitDOM.addEventListener('click', (e) => {
+    e.preventDefault();
+    let kreipinys = nameDOM.value === '' ? 'nepateikes vardo' : 'vardu ' + nameDOM.value;
+    let gerimas = kurisPazymetas(allGerimaiDOM) ? `${kurisPazymetas(allGerimaiDOM)} yra pasirinktas gerymas` : `nepasirinko gerimo`;
 
 
-buttonDOM.addEventListener('click', (event) => {
-    event.preventDefault();
-    spanDOM.innerText = inputDOM.value;
-    inputDOM.value = '';
+
+    orderDOM.innerText = `Uzsakovas ${kreipinys} ${arNoriu(sriubaDOM)} sriubos, ${arNoriu(pagrindinisPatiekalasDOM)} pagrindinio patiekalo, ${arNoriu(desertasDOM)} deserto ir ${gerimas}.`;
 })
-
-
-
-
-
-
-
-
 
 
