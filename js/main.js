36ae7f8f5db04b4d7606a4b79d8638d725ed1e4e
Donatas <donkuprys@gmail.com>
2021-06-09 17:58:02 +0300

    14,15

--- js/main.js
+++ js/main.js
@@ -1,34 +1,8 @@
-const lentaDOM = document.querySelector('.lenta');
-const namuRezultatasDOM = lentaDOM.querySelector('[data-komanda="namu"]');
-const sveciuRezultatasDOM = lentaDOM.querySelector('[data-komanda="sveciu"]');
 
-const aiksteleDOM = document.querySelector('.aikstele');
-const namuKomandaDOM = aiksteleDOM.querySelector('[data-komanda="namu"]');
-const taskaiNamuDOM = namuKomandaDOM.querySelectorAll('button');
-
-const sveciuKomandaDOM = aiksteleDOM.querySelector('[data-komanda="sveciu"]');
-const taskaiSveciuDOM = sveciuKomandaDOM.querySelectorAll('button');
-
-
-
-let namuKomandosTaskai = 0;
-
-for (let i = 0; i < taskaiNamuDOM.length; i++) {
-    const mygtukas = taskaiNamuDOM[i];
-    mygtukas.addEventListener('click', () => {
-        namuKomandosTaskai += i + 1;
-        namuRezultatasDOM.innerText = namuKomandosTaskai;
-    })
+function generuotiSocials() {
 
 }
 
-let sveciuKomandosTaskai = 0;
+const icons = ['facebook', 'twitter', 'linkedin'];
 
-for (let i = 0; i < taskaiSveciuDOM.length; i++) {
-    const mygtukas = taskaiSveciuDOM[i];
-    mygtukas.addEventListener('click', () => {
-        sveciuKomandosTaskai += i + 1;
-        sveciuRezultatasDOM.innerText = sveciuKomandosTaskai;
-    })
-
-}
+generuotiSocials('.socials', icons)
