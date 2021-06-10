f333ac7ae9cfed8fb07bd6a7b6b40919a383c9a5
Donatas <donkuprys@gmail.com>
2021-06-10 20:30:53 +0300

    menu-16 sprendimas su daug variantu

--- js/main.js
+++ js/main.js
@@ -1,32 +1,38 @@
+function renderNav(selector, data) {
+    //validation
 
-function generuotiSocials(selector, icons) {
-    if (selector === '' ||
-        typeof selector !== 'string') {
-        console.error('ERROR: netinkamas parametras')
-        return false;
-    }
-    if (!Array.isArray(icons) ||
-        icons.length === 0) {
-        console.error('ERROR: negali buti tuscias array')
-        return false;
-    }
-    const iconsDOM = document.querySelector(selector);
-    if (!iconsDOM) {
-        console.error('ERROR: nepavyko rasti elemento')
-        return false;
-    }
-
+    //logic
+    const DOM = document.querySelector(selector);
     let HTML = '';
-    for (let i = 0; i < icons.length; i++) {
-        if (typeof icons[i] === 'string' &&
-            icons[i] !== '') {
-            HTML += `<i class="fa fa-${icons[i]}"></i>`;
-            console.log(icons);
-        }
 
+    for (const i in data) {
+        const menuItem = data[i];
+        HTML += `<a href="${menuItem.href}">${menuItem.title}</a>`
     }
-    iconsDOM.innerHTML = HTML;
+
+    //post-logic validation
+
+    //result return
+    DOM.insertAdjacentHTML('beforeend', `<nav>${HTML}</nav>`);
 }
 
-const icons = ['facebook', 'twitter', 5, [], '', 'linkedin', 'instagram'];
-generuotiSocials('.socials', icons);
+const menu = [
+    {
+        href: '#',
+        title: 'Home'
+    },
+    {
+        href: '#',
+        title: 'Services'
+    },
+    {
+        href: '#',
+        title: 'About us'
+    },
+    {
+        href: '#',
+        title: 'Contact us'
+    }
+];
+
+renderNav('header', menu);
