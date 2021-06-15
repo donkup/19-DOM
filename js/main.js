5dc64874b96bafcc0cc16b37d76dcb1550064c26
Donatas <donkuprys@gmail.com>
2021-06-15 21:04:43 +0300

    19c

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-function time(start, end) {
-    for (let h = start; h < end; h++) {
-        for (let m = 0; m < 60; m += 15) {
-            console.log(`${h}:${m < 10 ? '0' + m : m}`);
-        }
+function time(start, end, step) {
+    const pamokuKiekis = Math.floor((end - start) * 60 / step);
+    console.log(pamokuKiekis + 1);
+    for (let i = start * 60; i <= end * 60; i += step) {
+        const h = Math.floor(i / 60);
+        const m = i - (h * 60);
+        console.log(i, `${h}:${m < 10 ? '0' + m : m}`);
+
     }
 }
-time(1, 7);
+time(8, 13, 45);
+time(1, 4, 31);
