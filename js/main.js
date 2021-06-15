268e6cee9a16b7749f07cd1499fbe6f8c048e956
Donatas <donkuprys@gmail.com>
2021-06-15 19:57:39 +0300

    19b ir 19cStart

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-function time() {
-    for (let h = 0; h < 24; h++) {
+function time(start, end) {
+    for (let h = start; h < end; h++) {
         for (let m = 0; m < 60; m += 15) {
-            console.log(`${h}:${m}`);
+            console.log(`${h}:${m < 10 ? '0' + m : m}`);
         }
     }
 }
-time();
+time(1, 7);
