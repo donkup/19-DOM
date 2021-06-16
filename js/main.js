3a216c67e02beaa0ef7481255d7ee8272229a823
Donatas <donkuprys@gmail.com>
2021-06-16 19:39:05 +0300

    20

--- js/main.js
+++ js/main.js
@@ -1,19 +1,8 @@
-function time(start, end, step) {
-    const pamokuKiekis = Math.floor((end - start) * 60 / step);
-    console.log(pamokuKiekis + 1);
-    let newEnd = end;
-    if (start > end) {
-        newEnd = 24 + end;
+function tortoGabalas(tKiekis, zKiekis, tSvoris) {
+    const zmPerTorta = Math.ceil(zKiekis / tKiekis);
+    return Math.floor(tSvoris / zmPerTorta * 1000);
 
-    }
-    for (let i = start * 60; i <= newEnd * 60; i += step) {
-        const h = Math.floor(i / 60);
-        const m = i - (h * 60);
-        const hMod = h % 24;
-        console.log(i, `${h % 24 < 10 ? '0' + hMod : hMod}:${m < 10 ? '0' + m : m}`);
-
-    }
 }
-// time(8, 13, 45);
-// time(1, 4, 31);
-time(22, 1, 45);
+
+tortoGabalas(3, 17, 1); //166
+
